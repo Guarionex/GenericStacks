d95afc629b5600df7e2f80a679be5a7642e392f9
Edwin <chespirana@gmail.com>
2020-03-29 23:54:04 -0500

    Basic Stack functionalities

--- Tests/StackTest.java
+++ Tests/StackTest.java
@@ -1,3 +1,5 @@
+import br.com.fidias.chance4j.Chance;
+import br.com.fidias.chance4j.ChanceException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -6,11 +8,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StackTest {
 
-    private Stack sut;
+    private Stack _sut;
+    private Chance _chance;
 
     @BeforeEach
     void setUp() {
-        sut = new Stack();
+        _sut = new Stack();
+        _chance = new Chance();
     }
 
     @Test
@@ -18,7 +22,41 @@ class StackTest {
     {
         var expectedSize = 0;
 
-        var actualSize = sut.size();
+        var actualSize = _sut.size();
+
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    void GivenSUT_WhenPushing_ThenSizeReturns1() throws ChanceException {
+        var expectedSize = 1;
+        var item = _chance.integer();
+
+        _sut.push(item);
+        var actualSize = _sut.size();
+
+        assertEquals(expectedSize, actualSize);
+    }
+
+    @Test
+    void GivenSUT_WhenPoping_ThenReturnsLastItem() throws ChanceException {
+        var expectedItem = _chance.integer();
+
+        _sut.push(expectedItem);
+        var actualItem = _sut.pop();
+
+        assertEquals(expectedItem, actualItem);
+    }
+
+    @Test
+    void GivenSUT_WhenPopingStackOfSize1_ThenSizeReturns0() throws ChanceException {
+        var expectedSize = 0;
+        var item = _chance.integer();
+
+        _sut.push(item);
+        _sut.pop();
+        var actualSize = _sut.size();
+
 
         assertEquals(expectedSize, actualSize);
     }
--- src/Stack.java
+++ src/Stack.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+
 public class Stack {
+    private ArrayList _stackArrayList;
+
+    public Stack()
+    {
+        _stackArrayList = new ArrayList();
+    }
+
     public int size() {
-        return 0;
+        return _stackArrayList.size();
+    }
+
+    public void push(Object item) {
+        _stackArrayList.add(item);
+    }
+
+    public Object pop() {
+        return _stackArrayList.remove(_stackArrayList.size() - 1);
     }
 }
