a43ed8cb25e49ec20c83702125c49526f19079ac
Edwin <chespirana@gmail.com>
2020-03-29 23:04:47 -0500

    FirstCommit

---
+++ Tests/StackTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void AssertsTrue()
+    {
+        assertTrue(true);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
